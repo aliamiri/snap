b63774cdcca594437911d5af7f95186e36ba63b9
Ali Amiri <aliamiri68@gmail.com>
2019-10-19 09:05:24 +0330

    Add an script to run kronem multiple times

---
+++ examples/kronem/run.bat
@@ -0,0 +1,5 @@
+seq 5 | xargs -Iz ./kronem in_1_16.txt
+seq 5 | xargs -Iz ./kronem in_16_1.txt
+seq 20 | xargs -Iz ./kronem in_4_4.txt
+seq 10 | xargs -Iz ./kronem in_2_8.txt
+seq 10 | xargs -Iz ./kronem in_8_2.txt
